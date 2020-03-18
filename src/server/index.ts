ae4476a313b9865ab30b326ceff1968eb566a5b5
Alexey Vedmedenko <vedmalex@gmail.com>
2020-03-18 23:37:22 +0300

    move firebase admin

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,10 @@
 import dataProviderFirebaseAdmin from './dataProviderFirebaseAdmin'
 import apolloLodashGrapqhQLPlugin from './lodash'
-import {
-  adminClient,
-  typeDefs as firebaseAdminTypeDef,
-  resolvers as firebaseAdminResolvers,
-} from './admin'
-import fireBaseAdminHandler from './admin'
+import { FirebaseAdmin, AclDirective } from './firebase-admin'
 
 export {
   apolloLodashGrapqhQLPlugin as ApolloLodashGrapqhQLPlugin,
   dataProviderFirebaseAdmin,
-  adminClient,
-  firebaseAdminTypeDef,
-  firebaseAdminResolvers,
-  fireBaseAdminHandler,
+  FirebaseAdmin,
+  AclDirective,
 }
